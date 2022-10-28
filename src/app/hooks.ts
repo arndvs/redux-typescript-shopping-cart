cdc8ba7d146c931082ed76481da258b8e56c66d3
Aaron Davis <aaron@ripemetrics.com>
2022-10-28 12:05:15 -0700

    hooks and cartslice updates

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -5,5 +5,6 @@ import type { RootState, AppDispatch } from "./store"; // import the RootState a
 // Redux provides the useDispatch and useSelector hooks to access the store's dispatch and state inside a React component.
 // But in order to have them aware of the types defined in the store.ts file, we need to re-export new functions that are type aware.
 
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>(); // create a custom hook that returns the AppDispatch type
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector; // create a custom hook that returns the RootState type
